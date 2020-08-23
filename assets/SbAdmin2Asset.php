04322fd768b573a4a06fd1a6a07fbe1431cf9b28
Arief Wijaya <hoaaah.arief@gmail.com>
2020-08-23 21:59:35 +0700

    Update SbAdmin2Asset.php

--- assets/SbAdmin2Asset.php
+++ assets/SbAdmin2Asset.php
@@ -15,7 +15,7 @@ class SbAdmin2Asset extends AssetBundle
     
     public $css=[
         'vendor/fontawesome-free/css/all.min.css',
-        'font-awesome/css/font-awesome.min.css',
+        'vendor/fontawesome-free/css/fontawesome.min.css',
         'https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i',
         'css/sb-admin-2.min.css'
     ];
